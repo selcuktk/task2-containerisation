88249d030b535252d504f408d317f6659cc0959e
selcuktk <157029095+selcuktk@users.noreply.github.com>
2025-05-17 01:04:56 +0300

    internship_run_classifier() added for endpoint

--- src/pred/image_classifier.py
+++ src/pred/image_classifier.py
@@ -19,3 +19,12 @@ def tf_run_classifier(image: str) -> Any:
     pred_results = tf_predict(img)
     pred_results["status_code"] = 200
     return pred_results
+
+# is: internship
+def internship_run_classifier(image: str) -> Any:
+    img = load_image(image)  # loading image
+    if img is None:
+        return None
+    input_batch = is_preprocess(img)  # preprocessing image
+    top_labels = is_predict(input_batch, model=None)  # prediction
+    return 1
