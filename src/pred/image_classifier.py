6477cf1dd3aa0852418129a3c91af300600bcf66
selcuktk <157029095+selcuktk@users.noreply.github.com>
2025-05-17 19:02:11 +0300

    cleaning old models related functions

--- src/pred/image_classifier.py
+++ src/pred/image_classifier.py
@@ -1,24 +1,6 @@
 from src.pred.models.is_pred import *
 from typing import Any
 
-
-def torch_run_classifier(image: str) -> Any:
-    img = load_image(image)  # loading image
-    if img is None:
-        return None
-    input_batch = torch_preprocess(img)  # preprocessing image
-    top_labels = torch_predict(input_batch, model=None)  # prediction
-    return top_labels[0]
-
-
-def tf_run_classifier(image: str) -> Any:
-    img = load_image(image)
-    if img is None:
-        return None
-    pred_results = tf_predict(img)
-    pred_results["status_code"] = 200
-    return pred_results
-
 # is: internship
 def internship_run_classifier(image: str) -> Any:
     img = load_image(image)  # loading image
